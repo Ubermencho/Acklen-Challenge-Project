dfd82421e29438df96b959f0ee61ab51730f864c
Oscar Flores <oflores482@gmail.com>
2020-07-20 13:33:01 -0600

    Developed the Header for the web-app
    This header will be the one present in pretty much all renders of the different routes

---
+++ collectionfrntend/src/Components/Header/Header.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {IoIosLogIn, IoIosHome, IoIosFolder} from 'react-icons/io';
+import {NavLink} from 'react-router-dom';
+import './Header.css';
+
+export default class Header extends Component{
+    constructor(){
+        super();
+        this.state={
+            counter:0
+        }
+        this.counterUdpate = this.counterUdpate.bind(this);
+        this.logoutOnClick = this.logoutOnClick.bind(this);
+    }
+
+    counterUdpate(e){
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({counter:this.state.counter+1});
+    }
+
+    logoutOnClick(e){
+        e.preventDefault();
+        e.stopPropagation();
+        this.props.auth.logout();
+    }
+
+    render(){
+        if(this.props.hide && true){
+            return null;
+        }
+        if(this.props.auth && this.props.auth.isLogged && true){
+            return(
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink to="/"><IoIosHome/>Home</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+            );
+        }else{
+            return(
+                <header>
+                    <nav>
+                        <ul>
+                        <li><NavLink to="/"><IoIosHome/>Home</NavLink></li>
+                        <li><NavLink to="/login"><IoIosLogIn/>Login</NavLink></li>
+                        <li><NavLink to="/signin"><IoIosLogIn/>Sign in</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+            );
+        }
+    }
+}
