8e9b34dc2ed35a8be32f0a150b6c3b5d6fd98b2e
Oscar Flores <oflores482@gmail.com>
2020-07-23 17:04:10 -0600

    Edited the Post method request's limit

--- collectionbckend/app.js
+++ collectionbckend/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -15,10 +16,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({limit:'50mb', extended:true}));
+app.use(bodyParser.urlencoded({limit:'50mb', extended:true}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
